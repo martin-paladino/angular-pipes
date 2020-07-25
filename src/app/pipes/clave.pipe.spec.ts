cc6e0594a724f21762c5f251825ae3cd753ea237
Martin Paladino <c.martin.pal@gmail.com>
2020-07-25 20:15:54 -0300

    Auto-generated commit

--- src/app/pipes/clave.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ClavePipe } from './clave.pipe';
-
-describe('ClavePipe', () => {
-  it('create an instance', () => {
-    const pipe = new ClavePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
